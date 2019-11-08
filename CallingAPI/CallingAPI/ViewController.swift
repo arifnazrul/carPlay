ec651d044bbfa33ecb4afaa135af1a6ca84567df
Arinze <aniebue.arinze@yahoo.com>
2019-11-08 18:01:17 +0100

    R_ConnectionAPI Made a basic Platform Request Successfully

--- CallingAPI/CallingAPI/ViewController.swift
+++ CallingAPI/CallingAPI/ViewController.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import Alamofire
+import OAuthSwift
 
 class ViewController: UIViewController {
 
@@ -25,15 +26,67 @@ class ViewController: UIViewController {
     }
     
     func callAPI() {
-        let url = URL(string: URLStr)
+        /*let url = URL(string: URLStr)
         Alamofire.request(url!, method: .get, parameters: nil, headers: nil).responseJSON { (response) in
             if let responseObj = response.value as? [[String: Any]] {
                 self.resultArray = self.parseJSON(userData: responseObj)
                 self.tblView.reloadData()
                 print(self.resultArray)
             }
+        }*/
+        
+        print("INIT REQUEST")
+        var oauthswift = OAuth1Swift(
+            consumerKey:    "johndeere-6y54WmBQS8leIBJDm6IHMCmb6aWmfZFKJ1K1ifpi",
+            consumerSecret: "4a1c91e5be67cbcaebc0e49116492754a7f9c7fed98d0a81630f31d43c196655"
+            
+        )
+        
+        
+        oauthswift.client.get("https://sandboxapi.deere.com/platform/",
+                              success: { response in
+                                print(response.dataString())
+        },
+                              failure: { error in
+                                print("FAIL")
         }
+        )
+        
+        print("\n\n")
+        
+        /*oauthswift.client.get("https://sandboxapi.deere.com/platform/organizations/449503/machines",
+                              success: { response in
+                                print(response.dataString())
+        },
+                              failure: { error in
+                                print("FAIL")
+        }
+        )*/
+        
+        /*var oauthswift = OAuth1Swift(
+            consumerKey:    "johndeere-6y54WmBQS8leIBJDm6IHMCmb6aWmfZFKJ1K1ifpi",
+            consumerSecret: "4a1c91e5be67cbcaebc0e49116492754a7f9c7fed98d0a81630f31d43c196655",
+            requestTokenUrl: "https://sandboxapi.deere.com/platform/oauth/request_token",
+            authorizeUrl:    "https://sandboxapi.deere.com/platform/oauth/authorize",
+            accessTokenUrl:  "https://sandboxapi.deere.com/platform/oauth/access_token"
+        )
+        // authorize
+        let handle = oauthswift.authorize(
+            withCallbackURL: URL(string: "https://sandboxapi.deere.com:443/platform")!,
+            success: { credential, response, parameters in
+                print(credential.oauthToken)
+                print(credential.oauthTokenSecret)
+                print(parameters["user_id"])
+                // Do your request
+        },
+            failure: { error in
+                print(error.localizedDescription)
+        }
+        )
+        */
+        
     }
+    
     
     func parseJSON(userData: [[String: Any]]) -> [Users] {
         var userArr = [Users]()
