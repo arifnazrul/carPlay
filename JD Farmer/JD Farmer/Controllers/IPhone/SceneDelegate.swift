3e3279ab35e46b317e74387af6e8a30eaf759ecb
Priom Biswas <p_biswas19@informatik.uni-kl.de>
2019-11-08 11:38:53 +0100

    changed scene delegate code

--- JD Farmer/JD Farmer/Controllers/IPhone/SceneDelegate.swift
+++ JD Farmer/JD Farmer/Controllers/IPhone/SceneDelegate.swift
@@ -19,8 +19,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-        // Create the SwiftUI view that provides the window contents.
-        window?.rootViewController = LoginController()
+              let contentView = ContentView()
+            // window?.rootViewController = LoginController()
+         
+                 // Use a UIHostingController as window root view controller.
+               if let windowScene = scene as? UIWindowScene {
+                  let window = UIWindow(windowScene: windowScene)
+                   window.rootViewController = UIHostingController(rootView: contentView)
+                   self.window = window
+                   window.makeKeyAndVisible()
+
 
         // Use a UIHostingController as window root view controller.
     }
@@ -56,3 +64,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+}
